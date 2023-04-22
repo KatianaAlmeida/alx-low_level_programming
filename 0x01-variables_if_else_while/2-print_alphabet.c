8cbde2f306c467104272595b9d73b6e568262305
KatianaAlmeida <Katianaalmeida48@gmail.com>
2023-04-22 12:39:58 -0700

    My commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game
+ * main - I sometimes suffer from insomnia. And when I can't fall asleep, 
+ * I play what I call the alphabet game
  * 
  * Return: Always 0 (Sucess)
  */
