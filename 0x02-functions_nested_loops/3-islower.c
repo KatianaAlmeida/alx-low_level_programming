637d577698b79f8f6dea2dbf4864f1da9ce3c402
KatianaAlmeida <Katianaalmeida48@gmail.com>
2023-04-25 14:21:35 -0700

    My commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * int _islower - islower function
- *
+ * _islower - check if character is lower
+ * @c: is the is the variable that hold the character/value
+ * Return: 1_if c is lowercase or 0_otherwise
  */
 
 int _islower(int c)
