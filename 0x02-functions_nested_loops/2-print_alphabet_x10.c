5b4cdaf5138eabdc48cf9d4a0bda826ea75e5a4b
KatianaAlmeida <Katianaalmeida48@gmail.com>
2023-04-25 14:03:30 -0700

    My commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,14 @@
 #include "main.h"
 #include "1-alphabet.c"
+
 /**
- * print_alphabet_x10 - 10x alphabet 
+ * print_alphabet_x10 - 10x alphabet
  *
  */
 
 void print_alphabet_x10(void)
 {
-	int counter;
-	counter = 0;
+	int counter = 0;
 
 	while (counter != 10)
 	{
