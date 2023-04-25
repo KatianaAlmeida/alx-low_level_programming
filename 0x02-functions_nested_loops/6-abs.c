d8df47f623b7b85e704635d73a2d511c6df05879
KatianaAlmeida <Katianaalmeida48@gmail.com>
2023-04-25 15:02:20 -0700

    My commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - calculates the absolute value of a number
+ * @n: hold the number to check
+ * Return: the absolute value of an integer
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
