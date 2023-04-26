399222c321fa5db7cc983542a73caf958e0f2707
KatianaAlmeida <Katianaalmeida48@gmail.com>
2023-04-26 13:23:11 -0700

    My commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,19 +13,19 @@ void print_to_98(int number)
 	{
 		for (x = number; x <= 98; x++)
 		{
-			if (x <= 98)
+			if (x != 98)
 				printf("%d, ", x);
 			else if (x == 98)
-				_putchar('\n');
+				printf("%d\n, ", x);
 		}
 	} else if (number >= 98)
 	{
 		for (y = number; y >= 98; y--)
 		{
-			if (y >= 98)
+			if (y != 98)
 				printf("%d, ", y);
 			else if (y == 98)
-				_putchar('\n');
+				printf("%d\n, ", x);
 		}
 	}
 }
