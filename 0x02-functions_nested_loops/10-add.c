30a95fd6b7d01e98509fa7cf900a23d224c17b56
KatianaAlmeida <Katianaalmeida48@gmail.com>
2023-04-26 12:59:02 -0700

    My commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @first_number: to be added to second_number
+ * @second_number: to be added to first_number
+ * Return: sum of first_number and second_number
+ */
+int add(int first_number, int second_number)
+{
+	int result;
+
+	result = first_number + second_number;
+
+	return (result);
+}
