486169861decf38801f7ba61d52735e3d5e9f61b
KatianaAlmeida <Katianaalmeida48@gmail.com>
2023-05-03 13:06:39 -0700

    My commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,11 +14,12 @@ void print_array(int *a, int n)
 	{
 		if (index == (n-1))
 		{
-			printf("%d", a[n-1]);
+			printf("%d", a[n -1]);
 		}
 		else
 		{
 		    printf("%d, ", a[index]);
 		}
 	}
+	printf("\n");
 }
