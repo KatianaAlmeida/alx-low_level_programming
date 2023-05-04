5cea0d0df08c19d32e5e513546be11ac9a9d3906
KatianaAlmeida <Katianaalmeida48@gmail.com>
2023-05-04 14:02:36 -0700

    My commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,8 +12,8 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int index = 0;
-
-	for (; index < n && src[j] != '\0'; index++)
+	
+	for (; index < n && src[index] != '\0'; index++)
 	{
 		dest[index] = src[index];
 	}
