b0baa9b489cf2fca538686f7a064106d6d4810aa
KatianaAlmeida <Katianaalmeida48@gmail.com>
2023-05-04 14:52:09 -0700

    My commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - change all lowercase to uppercase
+ * @n: pointer
+ *
+ * Return: n
+ */
+
+char *string_toupper(char *n)
+{
+	int index;
+	
+	for (index = 0; n[index] != '\0'; index)
+	{
+		if (n[index] >= 'a' && n[index] <= 'z')
+			n[index] = n[index] - 32;
+	}
+	return (n);
+}
