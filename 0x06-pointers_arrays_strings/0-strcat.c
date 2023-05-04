a7fede5fd2111735998456dde280bee5b1df13c9
KatianaAlmeida <Katianaalmeida48@gmail.com>
2023-05-04 13:11:33 -0700

    My commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that concatenates two strings.
+ * @dest - hold the input value
+ * @src - holds the input value
+ *
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int index1 = 0;
+	int index2 = 0;
+
+	while (dest[index1] != '\0')
+	{
+		index1++;
+	}
+
+	while (src[index2] != '\0')
+	{
+		dest[index1] = src[index2];
+		index1++;
+		index2++;
+	}
+
+	dest[index1] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contain files for the project 0x06. C - More pointers, arrays and strings.
