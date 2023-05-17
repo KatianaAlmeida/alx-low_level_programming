7e2370c71a95722d65e42667dc81ee6e9df9b093
KatianaAlmeida <Katianaalmeida48@gmail.com>
2023-05-17 07:08:27 -0700

    My commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	char *pointer;
+	int index2;
+	int index1;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (index1 = 0; str[index1] != '\0';)
+		index1++;
+
+	pointer = malloc(sizeof(char) * (index1 + 1));
+
+	if (pointer == NULL)
+		return (NULL);
+
+	for (index2 = 0; str[index2]; index2++)
+		aaa[index2] = str[index2];
+
+	return (pointer);
+}
