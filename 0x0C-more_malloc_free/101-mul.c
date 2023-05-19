fe0c3c599a997739bfcfc454983b0aae7b2d1a6f
KatianaAlmeida <Katianaalmeida48@gmail.com>
2023-05-19 19:53:34 +0000

    My commit

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+#define ERR_MSG "Error"
+
+/**
+ *  * is_digit - multiplies two positive number
+ *   * @s: string to be evaluated
+ *    *
+ *     * Return: 0 if a non-digit is found, 1 otherwise
+ *      */
+
