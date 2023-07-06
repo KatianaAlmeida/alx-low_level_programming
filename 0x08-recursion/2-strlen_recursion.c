501286bd3b0928bdf5d3e122de9053eeee493a4a
KatianaAlmeida <Katianaalmeid48@gmail.com>
2023-07-06 11:43:57 -0700

    My commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,14 +3,14 @@
 
 /**
  * _strlen_recursion - returns the length of a string.
- *
- * return: count
+ * @s: string to get the lenght from
+ * Return: string lenght
  */
 
 int _strlen_recursion(char *s)
 {
-	if(*s == '\0')
-		return 0;
+	if (*s == '\0')
+		return (0);
 	else
-		return 1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 }
