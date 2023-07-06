39bc5e658047381d66072d30a6c76b33d939f73f
KatianaAlmeida <Katianaalmeid48@gmail.com>
2023-07-06 13:33:11 -0700

    My commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial -  calculates the factorial of a given number.
+ * @s: string 
+ * Retrun: factorial of s
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0) {
+		return (-1);
+	}
+	return (n * factorial(n - 1));
+}
